ae9f9948abc333fb141870068b675043aee77dbc
Muhammadlaraib-codes <muhammadlaraib2762@gmail.com>
2026-02-12 12:25:45 +0500

    Some bugs fixed.

--- entities/players/Player_Node2D.gd
+++ entities/players/Player_Node2D.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 
 # Declare member variables here.
-var player_direction: = 0
+var player_direction_x_axis: = 0
+var player_direction_y_axis: = 0
 var player_speed: = 200
 var player_half_width = 16
 var bullet_scene = preload("res://entities/bullets/Bullet_Node2D.tscn")
@@ -25,18 +26,32 @@ func _process(delta):
 		get_parent().get_node("Bullets_Node").add_child(bullet_instance)
 		
 	if Input.is_action_pressed("PlayerMoveLeft"):
-		player_direction = -1
+		player_direction_x_axis = -1
 		
 	elif Input.is_action_pressed("PlayerMoveRight"):
-		player_direction = 1
+		player_direction_x_axis = 1
+		
+	elif Input.is_action_pressed("PlayerMoveDown"):
+		player_direction_y_axis = 1
+		
+	elif Input.is_action_pressed("PlayerMoveUp"):
+		player_direction_y_axis = -1
 		
 	else: 
-		player_direction = 0
+		player_direction_x_axis = 0
+		player_direction_y_axis = 0
 		
-	position.x = clamp(position.x + delta * player_direction * player_speed, player_half_width, 400 - player_half_width)
+		
+	position.x = clamp(position.x + delta * player_direction_x_axis * player_speed, player_half_width, 400 - player_half_width)
+	position.y = clamp(position.y + delta * player_direction_y_axis * player_speed, player_half_width + 20, 600 - player_half_width)
 	
 	
+
+		
+
+
 func _on_PlayerBoundary_Area2D_area_entered(area: Area2D):
 	if area.name == "EnemyBoundary_Area2D":
+		area.get_parent().queue_free()
+		queue_free()
 		get_tree().paused = true
-		
